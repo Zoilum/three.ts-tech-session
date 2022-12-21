f2728ffe320555520b150a46a53664cdeb8b4614
Thomas Sommavilla <thomsomma@gmail.com>
2022-12-21 19:14:38 +0100

    adds object animation

--- src/main.ts
+++ src/main.ts
@@ -16,21 +16,22 @@ const renderer = new THREE.WebGLRenderer({canvas: canvas!});
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.x = 1.5
-cube.position.y = -1.5
-cube.position.z = -1
-
-// QUIZ
-// cube.rotateX(180)
-// cube.rotateY(180)
-// ===
-// cube.rotateY(180)
-// cube.rotateX(180)
-// ?
 
 scene.add( cube );
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.render( scene, camera );
+
+const animate = () => {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  camera.lookAt(cube.position)
+
+  renderer.render( scene, camera );
+};
+
+animate();
 
 export {}
