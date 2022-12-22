91dcb5ddd9ba87fb5e7fb28f0422fd16ef87e43f
Thomas Sommavilla <thomsomma@gmail.com>
2022-12-22 16:39:33 +0100

    fixes proportions

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const sizes = {
   height: 500
 }
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas!});
